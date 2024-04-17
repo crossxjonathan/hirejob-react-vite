537dfc6a9357ace733f2f67f80141019e5a5f2fb
crossxjonathan <crossxjonathan@gmail.com>
2024-04-17 13:03:05 +0700

    landingpage1

--- hirejob-react-vite/src/main.jsx
+++ hirejob-react-vite/src/main.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import LandingPage from './pages/landing page/landingPage.jsx'
+import LandingPage from './pages/landing page/landingPage'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <LandingPage />
+    {/* <App /> */}
+    <LandingPage/>
   </React.StrictMode>,
 )
